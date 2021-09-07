cb92c45175b070e264211b00fae4b3e66a5fde90
Jamil Alabi <jamilalabi1@gmail.com>
2021-09-07 13:53:15 -0700

    hiii

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strlen - return the length of the string
- * @s: char type pointer
- * Return: length of string
- */
+* _strlen - return the length of the string
+* @s: char type pointer
+* Return: length of string
+*/
 int _strlen(char *s)
 {
 int len;
