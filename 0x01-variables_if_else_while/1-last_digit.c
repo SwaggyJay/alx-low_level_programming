091839378acaf44bf996f12ef45e2869ca9a7466
Jamil Alabi <jamilalabi1@gmail.com>
2021-10-15 14:53:30 -0700

    hiiii

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 h = n % 10;
 if (h > 5)
 {
-printf("Last digit of %d is %d and is grater than 5", n,h);
+printf("Last digit of %d is %d and is greater than 5\n", n, h);
 }
 else if (h == 0)
 {
-printf("Last digit of %d is %d and is 0", n,h);
+printf("Last digit of %d is %d and is 0\n", n, h);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n,h);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, h);
 }
  return 0;
 }
