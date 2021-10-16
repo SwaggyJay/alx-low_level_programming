02341c64d9ae51276daaf079d15ea8bfd14bf703
Jamil Alabi <jamilalabi1@gmail.com>
2021-10-15 17:35:22 -0700

    hi

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - main block
+*Program prints alphabet in lowcase, followed by a new line
+*Return: 0
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+putchar('\n');
+}
+return (0);
+}
