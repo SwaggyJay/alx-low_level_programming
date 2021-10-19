d1e78dc43dee1e67f47dff707cd2e97033488e35
Jamil Alabi <jamilalabi1@gmail.com>
2021-10-19 00:17:15 -0700

    hi

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - main block
+*prints all single digit numbers of base 10 starting from 0, seperated by a comma and a space
+*using putchar function and cant use char variable type
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
