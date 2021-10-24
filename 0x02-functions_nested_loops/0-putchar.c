ae7d80d72ca6b14d50997de4000de350ec0118ec
Jamil Alabi <jamilalabi1@gmail.com>
2021-10-24 06:07:54 -0700

    Jay

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,17 @@
-int main (void);
+#include "main.h"
+/**
+* main - Entry point
+* write a program tha prints Holberton, followed by a new line.
+* Return: Always (Success)
+*/
+int main()
 {
-putchar("_putchar")
+char *u = "_putchar";
+while (*u)
+{
+_putchar(*u);
+*u++;
+}
+_putchar('\n');
+return(0);
 }
