6cbe6e29e7cd3f4d1135f4e7d99523456413ef4d
Jamil Alabi <jamilalabi1@gmail.com>
2021-10-24 12:40:57 -0700

    haiii

---
+++ 0x02-functions_nested_loops/2-print_alphabetx10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_alphabet - a function that prints the alphabet in lowercase.
+*
+*Return: Always 0.
+*/
+void print_alphabetx10(void)
+{
+int i;
+int k
+for (k = 1; k<= 10; k++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
